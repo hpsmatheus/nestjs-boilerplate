46e4779f6ed4ae4c8142663bfb1fc5ec10094165
Matheus Henrique <mhps.matheus@gmail.com>
2024-02-20 21:23:43 -0300

    change swagger response example

--- src/modules/example/example.controller.ts
+++ src/modules/example/example.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import ExampleService from './example.service'
-import { ApiResponse, ApiTags } from '@nestjs/swagger'
-import { SwaggerResponse } from 'src/core/swagger-response'
+import {
+	ApiInternalServerErrorResponse,
+	ApiOkResponse,
+	ApiResponse,
+	ApiTags,
+} from '@nestjs/swagger'
 import ExampleResponse from 'src/typings/example/example.response.dto'
 import ExampleParams from 'src/typings/example/example.params.dto'
 
@@ -11,9 +15,7 @@ export default class ExampleController {
 	constructor(private readonly exampleService: ExampleService) {}
 
 	@Get()
-	@ApiResponse(SwaggerResponse.Ok(ExampleResponse))
-	@ApiResponse(SwaggerResponse.InputValidationError)
-	@ApiResponse(SwaggerResponse.InternalError)
+	@ApiOkResponse({ type: [ExampleResponse] })
 	public example(@Query() params: ExampleParams): ExampleResponse {
 		if (params.name === 'Maria') throw new Error('Invalid name!')
 		return this.exampleService.example(params.name)
